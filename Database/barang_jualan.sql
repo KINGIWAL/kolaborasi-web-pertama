221386f00fd12695c96cbc62bcdee8f968563d7d
KINGIWAL <iwalilham23@gmail.com>
2025-01-12 14:32:35 +0700

    ketinggalan satu

--- Database/barang_jualan.sql
+++ Database/barang_jualan.sql
@@ -4,12 +4,11 @@ CREATE TABLE `barang_jualan` (
     `Barang_Jualan` varchar(255) DEFAULT NULL,
     `Jumlah_Barang` int DEFAULT NULL,
     `Alamat_penjual` varchar(255) DEFAULT NULL,
-    `Toko_penjual` varchar(75) DEFAULT NULL,
     `Harga_barang` int DEFAULT NULL,
     `Gambar_barang` varchar(255) NOT NULL,
-    `Email` varchar(50) DEFAULT NULL,
     `Keterangan_barang` varchar(255) DEFAULT NULL,
     `Jumlah_terjual` int NOT NULL,
     `Rating_barang` int NOT NULL,
+    `Kategori` varchar(55) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 21 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
