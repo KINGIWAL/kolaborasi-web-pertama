300dbfb7f4eb6a0f8901dabfd2dec25c86eba041
KINGIWAL <iwalilham23@gmail.com>
2025-01-12 14:31:45 +0700

    perubahan pada database

--- Database/penjual.sql
+++ Database/penjual.sql
@@ -4,5 +4,9 @@ CREATE TABLE `penjual` (
     `Email` varchar(50) NOT NULL,
     `Nomor` varchar(20) NOT NULL,
     `Password` varchar(255) NOT NULL,
+    `Alamat` varchar(255) NOT NULL,
+    `Toko` varchar(55) DEFAULT NULL,
+    `Kota` varchar(55) NOT NULL,
+    `Photo_profil` varchar(255) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 7 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
